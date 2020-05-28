2c87fd78bd01aec7235f1331fe4b184a31715165
krbrennan <krb.brennan@gmail.com>
2020-05-27 20:37:54 -0400

    Updates

---
+++ precache-manifest.166d4b165e6f232b2e4f2c7cd3654294.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e47e422b59fa35fdd443927871b36a9",
+    "url": "/rent-a-dog/index.html"
+  },
+  {
+    "revision": "9401d6dab9201f4b164b",
+    "url": "/rent-a-dog/static/css/main.cbf59bb5.chunk.css"
+  },
+  {
+    "revision": "6f1ccfb693631710abaa",
+    "url": "/rent-a-dog/static/js/2.f5f8ccca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rent-a-dog/static/js/2.f5f8ccca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9401d6dab9201f4b164b",
+    "url": "/rent-a-dog/static/js/main.918c168a.chunk.js"
+  },
+  {
+    "revision": "a4f6cd25e5291e8036f6",
+    "url": "/rent-a-dog/static/js/runtime-main.8ad67607.js"
+  },
+  {
+    "revision": "243097edcb1675f91e8dd3909c34a263",
+    "url": "/rent-a-dog/static/media/happy-min.243097ed.png"
+  },
+  {
+    "revision": "1baebb4e652e75068c532757372dfc85",
+    "url": "/rent-a-dog/static/media/hero-img.1baebb4e.jpg"
+  }
+]);
